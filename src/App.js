c9d8f5aa880132b31f035e62d3cf081298f55018
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2022-04-09 15:46:42 -0300

    Atualização nos estilos e organização de arquivos e pastas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
-import  api  from "./services/api";
-import imgErro from './assets/personagem.png'
-
-import {  GlobalStyle,  Main, ContainerInput,  ExibiInfos, ImagemErro } from './assets/styles'
+import React, { useState } from "react"
+import  api  from "./services/api"
+import imgErro from './assets/imgs/personagem.png'
+import octocat from './assets/imgs/octocat.png'
+import  GlobalStyle  from'./assets/styles/GlobalStyle'
+import { 
+  Main, Header, Logo ,ContainerInput,  
+  ExibiInfos, ImagemErro
+} from './assets/styles/Styles'
 
 
 function App() {
@@ -27,51 +31,72 @@ function App() {
 
   const showInfo = () => {
     return (
-      <ExibiInfos>
-      <ul>
-        <li><img src={dados.avatar_url} alt='Imagem dono do perfil' className="imagem-usuario"/></li>
-        <li className='nome-usuario'>{dados.name}</li>
-        <li className='login-usuario'>{dados.login}</li>
-        <li className='descricao-usuario'>{dados.bio}</li>
-       
-       <div className='infos-conta'>
-          <li>Seguidores: {dados.followers}</li>
-          <li>Seguindo: {dados.following}</li>
-          <li>Repositórios: {dados.public_repos}</li>
-       </div>
-  
-      </ul>
-      </ExibiInfos>
+      
+        <ExibiInfos>
+          <ul>
+            <li><img src={dados.avatar_url} alt='Imagem dono do perfil' className="imagem-usuario"/></li>
+            <li className='nome-usuario'>{dados.name}</li>
+            <li className='descricao-usuario'>{dados.bio}</li>
+          
+          <div className='infos-conta-container'>
+            <div className="infos-conta-titulos">
+              <li>Seguidores</li>
+              <li>Seguindo</li>
+              <li>Repositórios</li>
+            </div>
+
+            <div className="infos-conta-dados">
+              <li>{dados.followers}</li>
+              <li>{dados.following}</li>
+              <li>{dados.public_repos}</li>
+            </div>
+          </div>      
+          </ul>
+        </ExibiInfos>
     )
   }
 
   const msgUserNaoEncontrado = () => {
     return (
-      <ExibiInfos>
-        <ImagemErro src={imgErro} />
-        <h3 style={{marginTop: '20px'}}>Usuário não encontrado</h3>
-      </ExibiInfos>
+      <>
+
+        <ExibiInfos>
+          <ImagemErro src={imgErro} />
+          <h3 style={{marginTop: '20px'}}>Usuário não encontrado</h3>
+        </ExibiInfos>
+      </>
     )
   }
 
+
+  
+
   return (
-    
-    <Main>
+    <>
       <GlobalStyle />
+      <Main>    
+      <Header>
+        <Logo>
+             <img src={octocat} alt="Imagem do octocat com um lupa"/>
+             <h1 className="titulo-logo">Github dev find</h1>             
+        </Logo>
+        <ContainerInput>
+          <input type='text' value={user} onChange={e => setUser(e.target.value)}/>    
+          <button type='submit' onClick={() => {
+            handleOnClick()
 
-      <ContainerInput>
-        <input type='text' value={user} onChange={e => setUser(e.target.value)}/>    
-        <button type='submit' onClick={() => {
-          handleOnClick()
+          }}>Search</button>
+        </ContainerInput>        
+      </Header>  
 
-        }}>Search</button>
-      </ContainerInput>
-      
       {exibirInfos ? showInfo() :''}
       {usuarioNaoEncontrado && msgUserNaoEncontrado()}
+</Main>
 
 
-    </Main>
+
+    </>
+
   );
 }
 
---
+++ src/assets/styles/Styles.js
@@ -0,0 +1,177 @@
+import styled from 'styled-components'
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin:  0;
+  padding:  0;
+  box-sizing:  border-box;
+}
+`
+const Main = styled.main`
+background-color: #605F5F;
+display:  flex;
+flex-direction : column;
+align-items: center; 
+
+height: 100vh; 
+padding-top: 5%; 
+`
+
+const Header = styled.header`
+    display: flex; 
+    flex-direction: column;
+    align-items: center; 
+
+    width: 40vw;
+    height: 28vh;
+    box-sizing: border-box;
+    padding: 30px 0;
+
+    background: #7E65A6;
+    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( 9px );
+    -webkit-backdrop-filter: blur( 9px );
+    border-radius: 10px;
+
+`
+const Logo =  styled.div`
+  display: flex;
+  align-items: flex-end;
+
+    h1 {
+      color: #fff;
+      font-family: 'Quicksand', sans-serif;
+      font-weight: light;
+      letter-spacing: 3px;
+    }
+
+    img {
+      width: 70px;
+    }
+`
+const ContainerInput = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+    padding-top: 40px;
+
+input {
+  background-color: rgba(255, 255, 255, 0.205);
+  border: none;
+  border-radius: 8px;
+  width: 330px;
+  height: 25px;
+  padding: 20px;
+  outline: none;
+  color:  #fff;
+}
+button {
+  background-color: rgba(255, 255, 255, 0.205);
+  border: none;
+  border-radius: 8px;
+  width: 120px;
+  height: 42px;
+  text-align: center;
+  cursor: pointer;
+  color:  #fff;
+
+}
+
+`
+const ExibiInfos = styled.section`
+  width: 40vw;
+  height: 53vh;
+
+  padding-top: 50px;
+  padding-bottom: 50px ;
+
+  margin-top: 50px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background: #7E65A6;
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 9px );
+  -webkit-backdrop-filter: blur( 9px );
+  border-radius: 10px;
+
+
+  ul {
+    width: 90%;
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+
+    list-style: none; 
+    color: #fff;
+
+    padding-top: 20px;
+  }
+
+  .imagem-usuario {
+    width: 100px;
+    clip-path: circle();
+  }
+
+  .nome-usuario {
+    font-size: 1.5rem;
+    font-family: 'Quicksand', sans-serif;
+    font-weight: bold;
+    
+    letter-spacing: 3px;
+    color: #fff;
+   
+    margin-top: 20px;
+
+    box-sizing: border-box;
+  }
+
+  .descricao-usuario {
+    border-radius: 25px;
+
+    margin-top: 25px;
+    margin-bottom: 25px;
+    
+    line-height: 30px;
+    text-align: center;
+  }
+
+  .infos-conta-container {
+    display: flex;
+    flex-direction: column; 
+
+    margin-top: 25px;
+    margin-bottom: 25px;
+
+    width: 90%;
+  }
+  
+
+  .infos-conta-titulos {
+      display: flex;
+      justify-content: space-around;
+    }
+
+
+   .infos-conta-dados {
+      display: flex;
+      justify-content: space-around;
+    }
+
+  }
+
+`
+const ImagemErro = styled.img`
+  width: 70%;
+
+`
+
+export { GlobalStyle, Main , Header , Logo ,ContainerInput,  ExibiInfos, ImagemErro }
